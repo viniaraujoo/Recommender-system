937bb1e356a94d61c510de6d7bda77ea37174a18
Vinicius Brandao <vinicius.brandao.araujo@ccc.ufcg.edu.br>
2018-07-25 13:50:34 -0300

    Modificando o main

--- main.py
+++ main.py
@@ -1,35 +1,73 @@
 # -*- coding: utf-8 -*-
 import webapp2
 import json
+import os
+from backend import get_top_5_movies_KNN, user_set, get_top_5_movies_SVD, get_top_5_neighbors,rmse_knn,rmse_svd
+
+def read_file(path):
+    path = os.path.dirname(os.path.realpath(__file__)) + '/frontend' + path
+    file_stream = open(path, 'r')
+    data = file_stream.read()
+    file_stream.close()
+    return data
+
+def get_content_type(file):
+    switch = {
+        'js': 'application/javascript',
+        'css': 'text/css',
+        'html': 'text/html',
+        'default': ''
+    }
+
+    file_type = file.split(".")[-1]
+    return switch.get(file_type, switch['default'])
 
 
-"""
-Este Handler gerência as requisições
-feitas a url /api/hello, nele é possível
-definir por meio de funções qualquer 
-verbo HTTP (GET, POST, PUT, DELETE...)
-"""
-class MainHandler(webapp2.RequestHandler):
+class ResultsHandler(webapp2.RequestHandler):
     def get(self):
         """
         Método para gerênciar requisições GET.
         """
-        message = {
-            'msg': 'Hello Horld!'
+        uid = self.request.get('uid')
+        response = {
+            'result_knn': get_top_5_movies_KNN(uid),
+            'result_svd': get_top_5_movies_SVD(uid),
+            'neighbors': sorted(list(map(int, get_top_5_neighbors(uid)))),
+            'rmse_knn':rmse_knn,
+            'rmse_svd': rmse_svd
         }
 
         self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
-        self.response.write(json.dumps(message))
+        self.response.write(json.dumps(response))
 
 
-"""
-O código abaixo utiliza o módulo WSGIApplication
-para definir os endpoints da api rest,
-ele recebe como parâmetro uma lista de tuplas,
-onde cada tupla contém uma url e a referência
-para o Handler que irá gerenciar todas as
-requisições feitas a url que ele é responsável.
-"""
-app = webapp2.WSGIApplication([
-    ('/api/hello', MainHandler)
-], debug=True)
+class UsersIdHandler(webapp2.RequestHandler):
+    def get(self):
+        response = {
+            'users_id': sorted(list(map(int, user_set)))
+        }
+
+        self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
+        self.response.write(json.dumps(response))
+
+
+class IndexHandler(webapp2.RequestHandler):
+    def get(self):
+        path = '/index.html'
+        data = read_file(path)
+
+        self.response.headers['Content-Type'] = get_content_type(path.split('/')[-1])
+        self.response.write(data)
+
+class StaticsFilesHandler(webapp2.RequestHandler):
+    def get(self):
+
+        try:
+            path = self.request.path
+            data = read_file(path)
+
+            self.response.headers['Content-Type'] = get_content_type(path.split('/')[-1])
+            self.response.write(data)
+        except:
+            self.response.status = 404
+        
